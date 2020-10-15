dfeade942fa0826c43755143f92bbcde6713da65
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-15 12:57:02 -0700

    Get http method and format output

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ const url = require('url');
 // The server should respond to all request with a string
 const server = http.createServer((req, res) => {
 	// Get url and parse
-	const parsedUrl = url.parse(req.url);
+	const parsedUrl = url.parse(req.url, true);
 
 	// Get path to url
 	const path = parsedUrl.pathname;
-	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-	// console.log('trimmed path ----> ', trimmedPath)
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    
+    // Get query string as object
+    var queryStringObject = parsedUrl.query;
+
+	// Get http method
+	const method = req.method.toUpperCase();
 
 	// Send response
 	res.end('Hello World\n');
 	// Log path requested
-	console.log(`Request received on path: ${trimmedPath}`);
+	console.log(
+		`<---------->\nRequest path: ${trimmedPath}\nMethod: ${method}`
+    );
+    console.log(
+        'Query String Object: ', queryStringObject, '\n<---------->\n'
+    );
 });
 
 // Start the server, and have it listen on port 3000
