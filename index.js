c63a8f074957063179e8f4c6f02eaf2ab3cf14ac
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-10-21 00:21:07 -0700

    Refactor to arrow functions

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const handlers = require('./lib/handlers');
 const helpers = require('./lib/helpers');
 
 // Instantiate the HTTP server
-const httpServer = http.createServer(function (req, res) {
+const httpServer = http.createServer((req, res) => {
 	unifiedServer(req, res);
 });
 
 // Start the HTTP server
-httpServer.listen(config.httpPort, function () {
+httpServer.listen(config.httpPort, () => {
 	console.log('The HTTP server is running on port ' + config.httpPort);
 });
 
@@ -28,17 +28,17 @@ const httpsServerOptions = {
 	key: fs.readFileSync('./https/key.pem'),
 	cert: fs.readFileSync('./https/cert.pem'),
 };
-const httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
 	unifiedServer(req, res);
 });
 
 // Start the HTTPS server
-httpsServer.listen(config.httpsPort, function () {
+httpsServer.listen(config.httpsPort, () => {
 	console.log('The HTTPS server is running on port ' + config.httpsPort);
 });
 
 // All the server logic for both the http and https server
-const unifiedServer = function (req, res) {
+const unifiedServer = (req, res) => {
 	// Parse the url
 	const parsedUrl = url.parse(req.url, true);
 
@@ -58,10 +58,10 @@ const unifiedServer = function (req, res) {
 	// Get the payload,if any
 	const decoder = new StringDecoder('utf-8');
 	let buffer = '';
-	req.on('data', function (data) {
+	req.on('data', (data) => {
 		buffer += decoder.write(data);
 	});
-	req.on('end', function () {
+	req.on('end', () => {
 		buffer += decoder.end();
 
 		// Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
@@ -80,7 +80,7 @@ const unifiedServer = function (req, res) {
 		};
 
 		// Route the request to the handler specified in the router
-		chosenHandler(data, function (statusCode, payload) {
+		chosenHandler(data, (statusCode, payload) => {
 			// Use the status code returned from the handler, or set the default status code to 200
 			statusCode = typeof statusCode == 'number' ? statusCode : 200;
 
